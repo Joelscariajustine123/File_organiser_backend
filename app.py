8c1a85b6df17b1149a7d26688df7795c31b376d3
Joel scaria Justine <84953776+Joelscariajustine123@users.noreply.github.com>
2025-10-23 13:08:41 +0530

    Update main execution block for app configuration

--- app.py
+++ app.py
@@ -169,5 +169,10 @@ def serve_file(filename):
 # --------------------
 # Run
 # --------------------
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default 5000 locally
+    app.run(host="0.0.0.0", port=port)
